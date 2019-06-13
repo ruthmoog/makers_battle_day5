c2f46ac876c3d84366c586c98b79b4d38b0df7d2
Ruth Baker <ruthmoog@gmail.com>
2019-06-13 16:49:43 +0100

    work in progress - this commit will break the build :(

--- app.rb
+++ app.rb
@@ -2,9 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  # get '/' do
+  #   "Testing infrastructure working!"
+  # end
+
   get '/' do
-    "Testing infrastructure working!"
+    @player_1 = params[:name_1]
+    @player_2 = params[:name_2]
+    erb(:index)
   end
+
+  post '/names' do
+    params[:name_1]
+    params[:name_2]
+    erb(:play)
+  end
+
 
   # this will start the server automatically is the app.rb file is run in the command line, ie `ruby app.rb`, so you don't have to start the server with eg `shotgun config.ru` :)
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -6,3 +6,18 @@ describe "homepage", type: :feature do
     page.should have_content 'Testing infrastructure working!'
   end
 end
+
+describe "enter player names and see them", type: :feature do
+# fill in name in form
+# submit the form
+# see names on screen
+    it 'allows players to enter their names & displays their names' do
+      visit '/'
+      fill_in('Name', with: 'Ruth')
+      fill_in('Name', with: 'Chris')
+      click_button('Submit')
+      page.should have_content 'Ruth Vs Chris'
+    end
+end
+
+
