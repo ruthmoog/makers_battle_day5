a1b5180ceb0db95f1ad6acf6b97115cbe4c4c116
Ruth Baker <ruthmoog@gmail.com>
2019-06-14 16:43:53 +0100

    Enable session and redirect form submission

--- app.rb
+++ app.rb
@@ -1,17 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb(:play)
+  post '/names' do
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect to('/play')
   end
 
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:play)
+  end
   # this will start the server automatically if the app.rb file is run in the command line, ie `ruby app.rb`, so you don't have to start the server with eg `shotgun config.ru` :)
   run! if app_file == $0
 end
