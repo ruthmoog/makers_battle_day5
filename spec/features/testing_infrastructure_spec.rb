9ff8dbeff8f2fac81007499ef66535fff57144e0
Aleksandra <ajmpawlik@gmail.com>
2019-06-14 17:49:46 +0100

    enabled viewing player 2's hit points, feature tested

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -7,3 +7,13 @@ describe "enter player names and see them", type: :feature do
       expect(page).to have_content 'Ruth Vs Chris'
     end
 end
+
+describe "see player's hit points", type: :feature do
+  it "allows player_1 to view player_2's hit points" do
+    visit '/'
+    fill_in('player_1', with: 'Ruth')
+    fill_in('player_2', with: 'Chris')
+    click_button('Submit')
+    expect(page).to have_content "Chris: 0 hit points"
+  end
+end
