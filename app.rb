18a96c29701181e0ccb546d4e70dab3432e8de61
Ruth Baker <ruthmoog@gmail.com>
2019-06-14 15:13:13 +0100

    fix bug, refactor, add rack to gemfile

--- app.rb
+++ app.rb
@@ -2,23 +2,17 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  # get '/' do
-  #   "Testing infrastructure working!"
-  # end
-
   get '/' do
-    @player_1 = params[:name_1]
-    @player_2 = params[:name_2]
+
     erb(:index)
   end
 
-  post '/names' do
-    params[:name_1]
-    params[:name_2]
+  post '/play' do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
     erb(:play)
   end
 
-
-  # this will start the server automatically is the app.rb file is run in the command line, ie `ruby app.rb`, so you don't have to start the server with eg `shotgun config.ru` :)
+  # this will start the server automatically if the app.rb file is run in the command line, ie `ruby app.rb`, so you don't have to start the server with eg `shotgun config.ru` :)
   run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,23 +1,9 @@
-# require_relative '../app'
-
-describe "homepage", type: :feature do
-  it 'displays message "Testing infrastructure working!"' do
-    visit '/'
-    page.should have_content 'Testing infrastructure working!'
-  end
-end
-
 describe "enter player names and see them", type: :feature do
-# fill in name in form
-# submit the form
-# see names on screen
     it 'allows players to enter their names & displays their names' do
       visit '/'
-      fill_in('Name', with: 'Ruth')
-      fill_in('Name', with: 'Chris')
+      fill_in('player_1', with: 'Ruth')
+      fill_in('player_2', with: 'Chris')
       click_button('Submit')
-      page.should have_content 'Ruth Vs Chris'
+      expect(page).to have_content 'Ruth Vs Chris'
     end
 end
-
-
